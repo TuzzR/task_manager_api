59fca2aed4863daae4d1c87cbd6dbf270f617489
TuzzR <tusharthakur2007@gmail.com>
2023-11-24 17:39:21 +0530

    update for creation date and added priority level based retrieval

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
-
+const getCurrentTimestamp = () => new Date().toISOString();
 let tasks = [];
 app.get("/tasks", (req, res) => {
-  res.json(tasks);
+  const { completed, sort } = req.query;
+  let filteredTasks = tasks;
+  if (completed !== undefined) {
+    const isCompleted = completed.toLowerCase() === "true";
+    filteredTasks = tasks.filter((task) => task.completed === isCompleted);
+  }
+  if (sort === "created") {
+    filteredTasks.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+  }
+  res.json(filteredTasks);
 });
 
 app.get("/tasks/:id", (req, res) => {
@@ -21,15 +30,13 @@ app.get("/tasks/:id", (req, res) => {
 });
 
 app.post("/tasks", (req, res) => {
-  const { title, description, completed } = req.body;
+  const { title, description, completed, priority } = req.body;
 
-  if (!title || !description || completed === undefined) {
-    res
-      .status(400)
-      .json({
-        error:
-          "Invalid request. Title, description, and completion status are required.",
-      });
+  if (!title || !description || completed === undefined || !priority) {
+    res.status(400).json({
+      error:
+        "Invalid request. Title, description, and completion status are required.",
+    });
     return;
   }
 
@@ -38,6 +45,8 @@ app.post("/tasks", (req, res) => {
     title,
     description,
     completed,
+    priority,
+    createdAt: getCurrentTimestamp(),
   };
 
   tasks.push(newTask);
@@ -51,7 +60,13 @@ app.put("/tasks/:id", (req, res) => {
   const taskIndex = tasks.findIndex((t) => t.id === taskId);
 
   if (taskIndex !== -1) {
-    tasks[taskIndex] = { ...tasks[taskIndex], title, description, completed };
+    tasks[taskIndex] = {
+      ...tasks[taskIndex],
+      title,
+      description,
+      completed,
+      priority,
+    };
     res.json(tasks[taskIndex]);
   } else {
     res.status(404).json({ error: "Task not found" });
@@ -62,6 +77,14 @@ app.delete("/tasks/:id", (req, res) => {
   const taskId = req.params.id;
   tasks = tasks.filter((t) => t.id !== taskId);
   res.json({ message: "Task deleted successfully" });
+});
+
+app.get("/tasks/priority/:level", (req, res) => {
+  const priorityLevel = req.params.level;
+  const tasksWithPriority = tasks.filter(
+    (task) => task.priority.toLowerCase() === priorityLevel.toLowerCase()
+  );
+  res.json(tasksWithPriority);
 });
 
 const port = process.env.PORT || 3000;
